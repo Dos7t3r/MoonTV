ade8a0d1193b6dd98e0b00049405ff62c29f91b2
Dos7t3r <132345955+Dos7t3r@users.noreply.github.com>
2025-06-24 13:01:05 -0400

    Update MobileHeader.tsx

--- src/components/MobileHeader.tsx
+++ src/components/MobileHeader.tsx
@@ -8,7 +8,7 @@ const MobileHeader = () => {
           href='/'
           className='text-2xl font-bold text-green-600 tracking-tight hover:opacity-80 transition-opacity'
         >
-          KanKanBot - 看看搜索引擎
+          KanKanBot
         </Link>
       </div>
     </header>
